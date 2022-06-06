050ded091673bb9d3f6a93147d452771e7c5f927
ramazan-dtas <60772908+ramazan-dtas@users.noreply.github.com>
2022-06-06 14:11:19 +0200

    m

--- CarStore.Client/CarStore/src/app/app-routing.module.ts
+++ CarStore.Client/CarStore/src/app/app-routing.module.ts
@@ -1,9 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NewuserpageComponent } from './newuserpage/newuserpage.component';
+import { AdminComponent } from './admin/admin.component';
+import { FrontpageComponent } from './frontpage/frontpage.component';
+import { LoginpageComponent } from './loginpage/loginpage.component';
+import { ProductpageComponent } from './productpage/productpage.component';
+import { ProfilpageComponent } from './profilpage/profilpage.component';
+import { SearchpageComponent } from './searchpage/searchpage.component';
+import { ProductpageDetailComponent } from './productpage/productpage-detail/productpage-detail.component';
 
 const routes: Routes = [
-  { path: 'newuser', component: NewuserpageComponent }
+  { path: 'newuser', component: NewuserpageComponent },
+  { path: 'frontpage', component: FrontpageComponent},
+  { path:'adminpage', component: AdminComponent},
+  { path:'loginpage', component: LoginpageComponent}
 ];
 
 @NgModule({
